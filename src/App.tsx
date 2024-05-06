26822884e04551a06ff0a080f764945809a9df28
Pavel Korolev <art.master.pk@gmail.com>
2024-05-06 17:35:36 +0300

    init: project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./scss/theme.scss";
+import "./calendar-data"
+
+function App() {
+  return (
+    <div className="calendar-body">
+      Calendar....
+    </div>
+  );
+}
+
+export default App;
